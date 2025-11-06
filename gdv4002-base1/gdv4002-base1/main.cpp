295bc69c2ad80682e74458745b2e77f24b899ff5
JoshuaCarter78 <st20334253@outlook.cardiffmet.ac.uk>
2025-11-06 17:38:34 +0000

    Tutorial 1-3

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -13,12 +13,39 @@ int main(void) {
 
 		printf("Cannot setup game window!!!\n");
 		return initResult; // exit if setup failed
+
+
 	}
 
 	//
 	// Setup game scene objects here
 	//
 	
+	/*addObject("player", glm::vec2(1.0f,1.0f), glm::radians(45.0f), glm::vec2(0.5f,1.0f), "Resources\\Textures\\mcblock01.png", TextureProperties::NearestFilterTexture());*/
+
+	addObject("player1");
+	addObject("player2");
+
+	GameObject2D* player1Object = getObject("player1");
+
+	if (player1Object != nullptr) {
+
+		player1Object->position = glm::vec2(-1.0f, 1.0f);
+		player1Object->orientation = glm::radians(-30.0f);
+		player1Object->size = glm::vec2(2.0f, 3.0f);
+		player1Object->textureID = loadTexture("Resources\\Textures\\bumblebee.png");
+	}
+
+	GameObject2D* player2Object = getObject("player2");
+
+	if (player2Object != nullptr) {
+
+		player2Object->position = glm::vec2(1.5f, 1.0f);
+		player2Object->orientation = glm::radians(45.0f);
+		player2Object->size = glm::vec2(1.0f, 0.5f);
+		player2Object->textureID = loadTexture("Resources\\Textures\\Player1_ship.png");
+	}
+
 
 	// Enter main loop - this handles update and render calls
 	engineMainLoop();
