65a2fec7f7c91e1358584c6e8154b5ff9eb060b1
Joshua Carter <joshuacarter2479@gmail.com>
2025-11-19 20:17:15 +0000

    Up to 8

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -1,5 +1,7 @@
 #include "Engine.h"
 #include "Keys.h"
+#include "Player.h"
+#include "Enemy.h"
 #include <bitset>
 #include <complex>
 using namespace std;
@@ -9,6 +11,8 @@ void myUpdate(GLFWwindow* window, double tDelta);
 void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 std::bitset<5> keys{ 0x0 };
+
+
 
 int main(void) {
 
@@ -27,6 +31,30 @@ int main(void) {
 	//
 	// Setup game scene objects here
 	//
+
+	GLuint playerTexture = loadTexture("Resources\\Textures\\player1_ship.png");
+
+	Player* mainPlayer = new Player(glm::vec2(-1.5f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), playerTexture, 1.0f);
+
+	addObject("player", mainPlayer);
+
+
+	GLuint enemyTexture = loadTexture("Resources\\Textures\\alien01.png");
+
+	// 2. Create enemy objects
+	Enemy* enemy1 = new Enemy(glm::vec2(0.0f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), enemyTexture, 0.0f, glm::radians(45.0f));
+
+	Enemy* enemy2 = new Enemy(glm::vec2(1.0f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), enemyTexture, 0.0f, glm::radians(90.0f));
+
+	Enemy* enemy3 = new Enemy(glm::vec2(2.0f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), enemyTexture, 0.0f, glm::radians(60.0f));
+
+	// Add enemy objects to the engine
+	addObject("enemy1", enemy1);
+	addObject("enemy2", enemy2);
+	addObject("enemy3", enemy3);
+
+
+
 	
 	/*addObject("player", glm::vec2(1.0f,1.0f), glm::radians(45.0f), glm::vec2(0.5f,1.0f), "Resources\\Textures\\mcblock01.png", TextureProperties::NearestFilterTexture());*/
 
@@ -53,15 +81,20 @@ int main(void) {
 	//	player2Object->textureID = loadTexture("Resources\\Textures\\Player1_ship.png");
 	//}
 
-	addObject("player", glm::vec2(-1.5f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), "Resources\\Textures\\player1_ship.png");
+	//addObject("player", glm::vec2(-1.5f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), "Resources\\Textures\\player1_ship.png");
 
-	addObject("enemy", glm::vec2(0.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
+	//addObject("enemy", glm::vec2(0.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
 
-	addObject("enemy", glm::vec2(1.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
+	//addObject("enemy", glm::vec2(1.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
 
-	addObject("enemy", glm::vec2(2.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
+	//addObject("enemy", glm::vec2(2.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
+	//++
+
+#
+	//setUpdateFunction(myUpdate);
 	
-	setUpdateFunction(myUpdate);
+		
+
 	setKeyboardHandler(myKeyboardHandler);
 
 	listGameObjectKeys();
@@ -77,62 +110,62 @@ int main(void) {
 	return 0;
 }
 
+//
+//float enemyPhase[3] = { glm::radians(0.0f), glm::radians(90.0f), glm::radians(270.0f) };
+//float enemyPhaseVelocity[3] = { glm::radians(90.0f),
+// glm::radians(90.0f),
+// glm::radians(90.0f) };
 
-float enemyPhase[3] = { glm::radians(0.0f), glm::radians(90.0f), glm::radians(270.0f) };
-float enemyPhaseVelocity[3] = { glm::radians(90.0f),
- glm::radians(90.0f),
- glm::radians(90.0f) };
-
-void myUpdate(GLFWwindow* window, double tDelta) {
-
-	/*float player1RotationSpeed = glm::radians(90.0f);
-	GameObject2D* player1 = getObject("player1");
-	player1->orientation += player1RotationSpeed * tDelta;*/
-
-	GameObjectCollection enemies = getObjectCollection("enemy");
-	for(int i = 0; i < enemies.objectCount; i++) {
-
-		enemies.objectArray[i]->position.y = sinf(enemyPhase[i]); // assume phase stored in radians so no conversion needed
-
-		enemyPhase[i] += enemyPhaseVelocity[i] * tDelta;
-	}
-
-
-	GameObject2D* player = getObject("player");
-	
-	static float playerSpeed = 1.0f; // distance per second
-	static float playerAngle = glm::radians(90.0f);
-	complex<float> i = complex<float>(0.0f, 1.0f);
-	auto c = exp(i * player->orientation);
-	
-
-
-
-
-	
-
-	if (keys.test(Key::W) == true) {
-
-		player->position += glm::vec2(c.real(), c.imag()) * (float)tDelta;
-		//player->position.x += 0.0f;
-		//player->position.y += playerSpeed * (float)tDelta;
-	}
-	if (keys.test(Key::S) == true) {
-
-		player->position.y -= playerSpeed * (float)tDelta;
-	}
-	if (keys.test(Key::A) == true) {
-
-		player->orientation += playerAngle * (float)tDelta;
-	}
-	if (keys.test(Key::D) == true) {
-
-		player->orientation -= playerAngle * (float)tDelta;
-	}
-
-
-
-}
+//void myUpdate(GLFWwindow* window, double tDelta) {
+//
+//	/*float player1RotationSpeed = glm::radians(90.0f);
+//	GameObject2D* player1 = getObject("player1");
+//	player1->orientation += player1RotationSpeed * tDelta;*/
+//
+//	GameObjectCollection enemies = getObjectCollection("enemy");
+//	for(int i = 0; i < enemies.objectCount; i++) {
+//
+//		enemies.objectArray[i]->position.y = sinf(enemyPhase[i]); // assume phase stored in radians so no conversion needed
+//
+//		enemyPhase[i] += enemyPhaseVelocity[i] * tDelta;
+//	}
+//
+//
+//	GameObject2D* player = getObject("player");
+//	
+//	static float playerSpeed = 1.0f; // distance per second
+//	static float playerAngle = glm::radians(90.0f);
+//	complex<float> i = complex<float>(0.0f, 1.0f);
+//	auto c = exp(i * player->orientation);
+//	
+//
+//
+//
+//
+//	
+//
+//	if (keys.test(Key::W) == true) {
+//
+//		player->position += glm::vec2(c.real(), c.imag()) * (float)tDelta;
+//		//player->position.x += 0.0f;
+//		//player->position.y += playerSpeed * (float)tDelta;
+//	}
+//	if (keys.test(Key::S) == true) {
+//
+//		player->position.y -= playerSpeed * (float)tDelta;
+//	}
+//	if (keys.test(Key::A) == true) {
+//
+//		player->orientation += playerAngle * (float)tDelta;
+//	}
+//	if (keys.test(Key::D) == true) {
+//
+//		player->orientation -= playerAngle * (float)tDelta;
+//	}
+//
+//
+//
+//}
 
 void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
